aea168326e6fc7efbbc7ea33befac5c0fb081423
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-06 00:17:23 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -81,10 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages]="Lisp"
-programmer_hash{:yukihiro_matsumoto}[:languages]="C"
-programmer_hash[:yukihiro_matsumoto]
+programmer_hash[:yukihiro_matsumoto]={}
     
 end
 
